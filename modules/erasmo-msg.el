55daedb3de5b9326c249ff4258a5279b7ed13c72
root <root@buencamino>
2023-06-17 13:59:30 +0800

    mail setup with mu and mu4e

--- modules/erasmo-msg.el
+++ modules/erasmo-msg.el
@@ -1,11 +1,39 @@
 ;; -*- lexical-binding: t; -*-
 
-;; particulars----------
+;;; particulars
 
 (setq user-full-name erasmo-env-user-full-name)
 (setq user-mail-address erasmo-env-user-mail-address)
 
-;; IRC----------
+;;; mail
+
+(use-package mu4e
+  :ensure nil
+  :defer 20 ; Wait until 20 seconds after startup
+  :config
+
+  ;; This is set to 't' to avoid mail syncing issues when using mbsync
+  (setq mu4e-change-filenames-when-moving t)
+
+  ;; Refresh mail using isync every 10 minutes
+  (setq mu4e-update-interval (* 10 60))
+  (setq mu4e-get-mail-command "mbsync -a")
+  (setq mu4e-maildir "/mnt/d/mail/94tian@gmail.coma")
+
+  ;; (setq mu4e-drafts-folder "/[Gmail]/Drafts")
+  ;; (setq mu4e-sent-folder "/[Gmail]/Sent Mail")
+  ;; (setq mu4e-refile-folder "/[Gmail]/All Mail")
+  ;; (setq mu4e-trash-folder "/[Gmail]/Trash")
+
+  ;; (setq mu4e-maildir-shortcuts
+  ;;       '(("/Inbox" . ?i)
+  ;;         ("/[Gmail]/Sent Mail" . ?s)
+  ;;         ("/[Gmail]/Trash" . ?t)
+  ;;         ("/[Gmail]/Drafts" . ?d)
+  ;;         ("/[Gmail]/All Mail" . ?a)))
+  )
+
+;;; IRC
 
 (require 'erc)
 
@@ -60,11 +88,11 @@
   ;; disable logging
 (customize-set-variable 'erc-log-channels-directory nil)
 
-;; telegram----------
+;;; telegram
 ;; installed via guix
 
 
-;; news----------
+;;; news
 (use-package gnus
   :custom
   (gnus-select-method '(nntp "news.eternal-september.org")))
