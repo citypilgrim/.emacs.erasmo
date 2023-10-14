f6288ae1566260b454499983a4af32aba470590d
root <root@buencamino>
2023-10-14 12:57:04 +0800

    switched whisper.el to straight instead of submodule

--- modules/erasmo-ai.el
+++ modules/erasmo-ai.el
@@ -1,17 +1,7 @@
 ;; -*- lexical-binding: t; -*-
 
-;; TODO do without manual installation of whisper.el
-;; requires dependency whisper.cpp to be built.
-;; Building is automatic, but requires linux-libc-dev
-;; which is installed outside of guix.
-;; Upon initial start up of whisper, it prompts for
-;; building and retrieval of model, the latter
-;; requiring internet connection
-(add-to-list 'load-path
-             (concat user-emacs-directory
-                     "site-packages/whisper.el"))
-(require 'whisper)
 (use-package whisper
+  :straight '(:type git :host github :repo "natrys/whisper.el")  
   :ensure nil
   :bind ("<f5>" . whisper-run)
   :config
