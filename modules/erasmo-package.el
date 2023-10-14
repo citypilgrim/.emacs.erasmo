d20f3aef3387bde5b923e93759a00dc392f27747
root <root@buencamino>
2023-10-14 12:53:59 +0800

    added codeium

--- modules/erasmo-package.el
+++ modules/erasmo-package.el
@@ -17,4 +17,19 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+;; straight.el to install those packages not published on melpa
+;; treat this as a last resort
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
 (provide 'erasmo-package)
